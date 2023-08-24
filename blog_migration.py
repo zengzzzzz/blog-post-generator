fca179731e0926518c4857fa8481317b3ce1e0b2
zengzzzzz <mrzengzzzz@gmail.com>
2023-08-24 17:51:45 +0800

    init script

---
+++ blog_migration.py
@@ -0,0 +1,35 @@
+import os
+import json
+import re
+from datetime import datetime
+
+# Read JSON file
+with open('posts_example.json', 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+
+# Create directory if it doesn't exist
+if not os.path.exists('markdown_files'):
+    os.mkdir('markdown_files')
+
+# Iterate through each blog post
+for post in data:
+    title = post['Title']
+    content = post['Body']
+    date_added = datetime.strptime(post['DateAdded'], '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
+    categories = ''
+    tags = 'zengzzzzz-blog'
+    title = title.replace('/', '-')
+    
+    # Clean HTML tags while retaining line breaks
+    content = re.sub(r'<.*?>', '', content)
+    content = content.replace('\n', '  \n')  # Markdown line breaks require two trailing spaces
+
+    # Generate Markdown content
+    markdown_content = f"---\nlayout: post\ntitle:  \"{title}\"\ndate:   {date_added}\ncategories: {categories}\ntags:  {tags}\n---\n\n* content\n{{:toc}}\n\n{content}\n"
+
+    # Write to Markdown file
+    markdown_file_path = os.path.join('markdown_files', f'{date_added[:10]}-{title}.md')
+    with open(markdown_file_path, 'w', encoding='utf-8') as markdown_file:
+        markdown_file.write(markdown_content)
+
+print("Markdown file generation complete!")
