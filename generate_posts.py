c03b1fb2703de4b2876ae21e63b826a84282afeb
zengzh <zengzh@akulaku.com>
2023-09-01 14:01:06 +0800

    add url imgs

--- generate_posts.py
+++ generate_posts.py
@@ -2,6 +2,7 @@ import os
 import json
 import re
 from datetime import datetime
+import urllib.request
 
 # Read JSON file
 with open('posts_example.json', 'r', encoding='utf-8') as json_file:
@@ -11,6 +12,9 @@ with open('posts_example.json', 'r', encoding='utf-8') as json_file:
 if not os.path.exists('markdown_files'):
     os.mkdir('markdown_files')
 
+if not os.path.exists('imgs'):
+    os.mkdir('imgs')
+
 # Iterate through each blog post
 for post in data:
     title = post['Title']
@@ -19,10 +23,20 @@ for post in data:
     categories = ''
     tags = 'zengzzzzz-blog'
     title = title.replace('/', '-')
-    
+
     # Clean HTML tags while retaining line breaks
     content = re.sub(r'<.*?>', '', content)
     content = content.replace('\n', '  \n')  # Markdown line breaks require two trailing spaces
+    image_urls = re.findall(r'src="(.*?\.(png|jpg))"', content)  # get img urls
+
+    # Generate img
+    if image_urls:
+        image_count = 1
+        for image_url in image_urls:
+            image_name = f"{title}-{image_count}"
+            image_path = os.path.join('imgs', image_name)
+            urllib.request.urlretrieve(image_url, image_path)
+            image_count += 1
 
     # Generate Markdown content
     markdown_content = f"---\nlayout: post\ntitle:  \"{title}\"\ndate:   {date_added}\ncategories: {categories}\ntags:  {tags}\n---\n\n* content\n{{:toc}}\n\n{content}\n"
